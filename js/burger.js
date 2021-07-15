75d28592d934bf392f81575ad358772736289b8d
khamidullo-sheikh <xmirzaev@list.ru>
2021-07-15 22:21:04 +0500

    initial commit

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,12 @@
 let menu = document.querySelector(".navbar");
-let toggle = menu.querySelector(".toggler");
-toggle.addEventListener("click", function(){
-    menu.classList.toggle("menu");
+let toggle_open = menu.querySelector(".toggler-open");
+let toggle_close = menu.querySelector(".toggler-close")
+toggle_open.addEventListener("click", function(){
+    menu.classList.remove("menu-close");
+    menu.classList.add("menu-open");
+});
+
+toggle_close.addEventListener("click", function(){
+    menu.classList.add("menu-close");
+    menu.classList.remove("menu-open");
 });
